0fc34332e68c4a7210a89ca9831217d43b03e86f
cristiandramu <christian.dramu@gmail.com>
2023-01-08 21:47:57 +0000

    modified styling and added Player has won function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 let playerText = document.getElementById('player text');
 let restartBtn = document.getElementById('restartBtn');
 let boxes = Array.from(document.getElementsByClassName('box'));
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winner-blocks');
 
 /*
     ============================================
@@ -27,10 +28,44 @@ function boxClicked(e){
         spaces[id]= currentPlayer;
         e.target.innerText = currentPlayer;
 
+        if(playerWon() !==false){
+            playerText= '${currentPlayer} has won!'
+            let winning_blocks = playerWon();
+
+            winning_blocks.map( box => boxes[box].style.backgroundColor=winnerIndicator);
+            return
+
+        }
+
         currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT; 
     }
 }
 
+/*
+    ============================================
+      Player Has Won Function.
+    ============================================
+    */
+    const winningCombo = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+    function playerWon(){
+        for (const condition of winningCombo){
+            let [a, b, c] = condition;
+
+            if(spaces[a] && (spaces[a]== spaces[b] && spaces[a] == spaces[c])){
+                return[a,b,c]
+            }
+        }
+        return false;
+    }
 /*
     ============================================
       Restart Button Function.
@@ -44,8 +79,9 @@ function boxClicked(e){
 
         boxes.forEach(box =>{
             box.innerText = '';
+            box.style.backgroundColor='';
         })
-
+        playerText = 'Tic Tac Toe';
         currentPlayer = X_TEXT;
     }
 startGame();
