4786f6697a9daa264d0fc5f9ba779e13d5313c5a
cristiandramu <christian.dramu@gmail.com>
2023-01-08 20:38:28 +0000

    wiriting Java code to import elements and write function to take turns for players

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+let playerText = document.getElementById('player text');
+let restartBtn = document.getElementById('restartBtn');
+let boxes = Array.from(document.getElementsByClassName('box'));
+
+
+const O_TEXT = "O";
+const X_TEXT = "X";
+let currentPlayer = X_TEXT;
+let spaces = Array(9).fill(null);
+
+const startGame = () =>{
+    boxes.forEach(box => box.addEventListener('click', boxClicked));
+}
+
+function boxClicked(e){
+    const id = e.target.id;
+
+    if(!spaces[id]){
+        spaces[id]= currentPlayer;
+        e.target.innerText = currentPlayer;
+
+        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT; 
+    }
+}
+
+startGame();
