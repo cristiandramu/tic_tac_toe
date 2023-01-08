57d633c578cf09a184131426fd57e602fa61add2
cristiandramu <christian.dramu@gmail.com>
2023-01-08 23:02:28 +0000

    added draw function to the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,58 @@
-let playerText = document.getElementById('player text');
-let restartBtn = document.getElementById('restartBtn');
-let boxes = Array.from(document.getElementsByClassName('box'));
-let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winner-blocks');
+let playerText = document.getElementById('playerText')
+let restartBtn = document.getElementById('restartBtn')
+let boxes = Array.from(document.getElementsByClassName('box'))
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winner-blocks')
 
 /*
     ============================================
       Define event Listener 
     ============================================
     */
-const O_TEXT = "O";
-const X_TEXT = "X";
-let currentPlayer = X_TEXT;
-let spaces = Array(9).fill(null);
+const O_TEXT = "O"
+const X_TEXT = "X"
+let currentPlayer = X_TEXT
+let spaces = Array(9).fill(null)
+let count_plays = 0
 
 const startGame = () =>{
-    boxes.forEach(box => box.addEventListener('click', boxClicked));
+    boxes.forEach(box => box.addEventListener('click', boxClicked))
 }
 /*
     ============================================
-      SwitchTurn Function.
+      SwitchTurn & Player Has Won Function.
     ============================================
     */
-function boxClicked(e){
-    const id = e.target.id;
+    function boxClicked(e){
+    const id = e.target.id
 
-    if(!spaces[id]){
-        spaces[id]= currentPlayer;
-        e.target.innerText = currentPlayer;
+    if(!spaces[id] && count_plays < 9){
+        spaces[id]= currentPlayer
+        e.target.innerText = currentPlayer
 
         if(playerWon() !==false){
-            playerText= '${currentPlayer} has won!'
-            let winning_blocks = playerWon();
+            playerText.innerHTML = '${currentPlayer} has won!'
+            let winning_blocks = playerWon()
+            count_plays = 10
 
-            winning_blocks.map( box => boxes[box].style.backgroundColor=winnerIndicator);
+            winning_blocks.map( box => boxes[box].style.backgroundColor=winnerIndicator)
             return
 
         }
-
-        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT; 
+        count_plays++
+        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT 
     }
-}
+    if(count_plays ===9){
+        playerText.innerHTML= 'Draw Game!'
+        boxes.forEach(box => box.style.color = drawIndicator)
+    }
+    }
 
 /*
     ============================================
-      Player Has Won Function.
+      Check for winning combos.
     ============================================
     */
+
     const winningCombo = [
         [0,1,2],
         [3,4,5],
@@ -58,13 +65,13 @@ function boxClicked(e){
     ]
     function playerWon(){
         for (const condition of winningCombo){
-            let [a, b, c] = condition;
+            let [a, b, c] = condition
 
             if(spaces[a] && (spaces[a]== spaces[b] && spaces[a] == spaces[c])){
                 return[a,b,c]
             }
         }
-        return false;
+        return false
     }
 /*
     ============================================
@@ -72,16 +79,18 @@ function boxClicked(e){
     ============================================
     */
 
-    restartBtn.addEventListener('click' , restart);
+    restartBtn.addEventListener('click' , restart)
 
     function restart(){
-        spaces.fill(null);
-
-        boxes.forEach(box =>{
-            box.innerText = '';
-            box.style.backgroundColor='';
+        spaces.fill(null)
+        count_plays = 0
+        boxes.forEach(box => {
+            box.innerText = ''
+            box.style.backgroundColor=''
+            box.style.color = ''
         })
-        playerText = 'Tic Tac Toe';
-        currentPlayer = X_TEXT;
+        playerText.innerHTML = 'Tic Tac Toe'
+        currentPlayer = X_TEXT
     }
-startGame();
+
+startGame()
